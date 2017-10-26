7a8d1f39ad9f0bafa2f5a54d0df692c18622d2d2
tonningp <tonningp@cis.vvc.edu>
2017-10-25 18:44:16 -0700

    update

---
+++ shifts/asm_main.asm
@@ -0,0 +1,36 @@
+;
+; file: asm_main.asm
+
+%include "asm_io.inc"
+;
+; initialized data is put in the .data segment
+;
+segment .data
+
+; uninitialized data is put in the .bss segment
+;
+segment .bss
+
+;
+; code is put in the .text segment
+;
+segment .text
+        global  asm_main
+asm_main:
+        enter   0,0               ; setup routine
+        pusha
+        mov ax,0xc123
+        shl ax,1
+        shr ax,1
+        shr ax,1
+        mov ax,0xc123
+        shl ax,2
+        mov cl,3
+        shr ax,cl
+
+        popa
+        mov     eax, 0            ; return back to C
+        leave                     
+        ret
+
+
